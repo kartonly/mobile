bdc6ebeee2a7098d6333823a00fd342616c607dd
kartonly <kartonly@yandex.ru>
2022-02-25 22:58:17 +0300

    refactorx2

--- app/src/main/java/com/example/lesson1s2/data/sources/RemoteDataSource.kt
+++ app/src/main/java/com/example/lesson1s2/data/sources/RemoteDataSource.kt
@@ -1,11 +1,14 @@
 package com.example.lesson1s2.data.sources
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.liveData
 import com.example.lesson1s2.data.CurrencyApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import com.example.lesson1s2.data.CurrencyRepository
+import com.example.lesson1s2.data.CurrencyResponse
+import kotlinx.coroutines.*
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,7 +16,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.coroutines.CoroutineContext
 
 class RemoteDataSource {
-        suspend fun getSource(){
+    val _result = MutableLiveData<CurrencyResponse>()
+    val result: MutableLiveData<CurrencyResponse> = _result
+        suspend fun getSource() {
             val interceptor = HttpLoggingInterceptor()
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
             val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
@@ -26,13 +31,16 @@ class RemoteDataSource {
 
             val service = retrofit.create(CurrencyApi::class.java)
 
+
             suspend fun getValues() = coroutineScope{
                 launch(Dispatchers.IO as CoroutineContext){
                     val cur = service.getCurrencies()
-                    Log.d("MY_TAG","$cur")
+                    _result.value = cur
                 }
             }
             getValues()
+
+
 
 //            GlobalScope.launch(Dispatchers.IO as CoroutineContext){
 //                val cur = service.getCurrencies()
